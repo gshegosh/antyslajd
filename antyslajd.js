a1604c7bd78034a6d3f73b0642737d4799a9d244
Grzegorz Krugły <gk@krop.pl>
2017-11-22 22:11:19 +0100

    progress

--- antyslajd.js
+++ antyslajd.js
@@ -7,6 +7,7 @@ function Antyslajd() {
     this.rule = this.matchRule();
     if (this.rule) {
       console.log(this.rule.name, this.rule);
+      this.showBtn(true);
       this.load(location.href);
     }
   };
@@ -31,12 +32,11 @@ function Antyslajd() {
         return;
       }
     }
-    this.render();
+    this.showBtn();
   };
 
   this.load = (url) => {
     const basedUrl = this.baseUrl(url);
-
     if (!this.urls.includes(basedUrl)) {
       this.urls.push(basedUrl);
       const req = new XMLHttpRequest();
@@ -53,13 +53,32 @@ function Antyslajd() {
       req.open('GET', basedUrl);
       req.send();
     } else {
-      this.render();
+      this.showBtn();
     }
   };
-  
+
+  this.showBtn = (preparing) => {
+    const abtn = document.querySelector('#antyslajdbtn');
+    if (abtn) abtn.remove();
+
+    const placeholder = document.querySelector(this.rule.placeholderSelector);
+    const rect = placeholder.getBoundingClientRect();
+    const btn = document.createElement('a');
+    btn.setAttribute('id', 'antyslajdbtn');
+    if (!preparing) btn.setAttribute('class', 'ready');
+    btn.innerHTML = preparing ? 'Anty...' : 'Antyslajd';
+    btn.style.left = rect.left + 'px';
+    btn.style.top = rect.top + 'px';
+    if (!preparing) btn.onclick = this.render;
+    document.querySelector('body').appendChild(btn);    
+  }  
+
   this.baseUrl = (url) => url.startsWith('http') ? url : (window.location.protocol + '//' + window.location.host + url);
 
   this.render = () => {
+    const btn = document.getElementById('antyslajdbtn');
+    if (btn) btn.remove();
+
     // try sorting on gallery page number extracted from URL
     this.urls.sort();
     const commonUrlStart = this.commonStart(this.urls[0], this.urls[this.urls.length - 1]);
@@ -69,24 +88,27 @@ function Antyslajd() {
     const placeholder = document.querySelector(this.rule.placeholderSelector);
     placeholder.innerHTML = '';
 
-    let tmp = document.querySelectorAll(this.rule.removeFromPage);
-    for (let i = 0; i < tmp.length; i++) tmp[i].remove();
+    if (this.rule.removeFromPage) {
+      const tmp = document.querySelectorAll(this.rule.removeFromPage);
+      for (let i = 0; i < tmp.length; i++) tmp[i].remove();
+    }
 
-
-    // tmp = document.querySelector(this.rule.itemSelector);
-    // tmp = tmp.querySelectorAll(this.rule.removeFromContainer);
-    // for (let i = 0; i < tmp.length; i++) tmp[i].remove();
+    if (this.rule.elementsToEmpty) {
+      const tmp = document.querySelectorAll(this.rule.elementsToEmpty);
+      for (let i = 0; i < tmp.length; i++) tmp[i].innerHTML = '';
+    }
 
     let slide = 0;
     this.urls.forEach(url => {
-      const item = this.urlHtmls[url].querySelector(this.rule.itemSelector);
-
-      const tmp = item.querySelectorAll(this.rule.removeFromContainer);
+      console.log('a-1');
+      const tmp = this.urlHtmls[url].querySelectorAll(this.rule.removeFromContainer);
       for (let i = 0; i < tmp.length; i++) tmp[i].remove();
+
+      const item = this.urlHtmls[url].querySelector(this.rule.itemSelector);
 
       const h3 = document.createElement('h3');
       h3.setAttribute('class', 'antyslajd');
-      h3.innerHTML = 'Antyslajd ' + (++slide) + ' / ' + this.urls.length;
+      h3.innerHTML = '<span class="antygreen">Anty</span>slajd ' + (++slide) + ' / ' + this.urls.length;
 
       placeholder.insertAdjacentElement('beforeend', h3);
       placeholder.insertAdjacentElement('beforeend', item);
@@ -135,82 +157,3 @@ function Antyslajd() {
 }
 
 new Antyslajd().main();
-
-/*
-
-
-function load(seenUrls, htmls, rule, url) {
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', function() {
-    var sanitizedHtml = DOMPurify.sanitize(this.responseText, {
-      SAFE_FOR_JQUERY: true,
-      WHOLE_DOCUMENT: true,
-      RETURN_DOM: true,
-      FORBID_TAGS: ['script'],
-      SANITIZE_DOM: false
-    });
-    htmls.push(sanitizedHtml);
-
-    const next = sanitizedHtml.querySelector(rule.nextSelector);
-    const nextUrl = next.getAttribute('href');
-    console.log('aaa', next, !seenUrls.includes(nextUrl));
-    if (next && !seenUrls.includes(nextUrl)) {
-      console.log('l22', load);
-      seenUrls.push(nextUrl);
-      load(seenUrls, htmls, rule, nextUrl);
-    } else {
-      console.log('ll3');
-      render(htmls, rule);
-    }
-  });
-  req.open('GET', baseUrl(url));
-  req.send();
-}
-
-function render(htmls, rule) {
-  let divs = [];
-  divs.push(document.querySelector(rule.container));
-  for (let i = 0; i < htmls.length; i++) {
-    divs.push(htmls[i].querySelector(rule.container));
-  }
-  console.log(42, divs);
-
-  const tmp = document.querySelectorAll('#antyslajd');
-  for (let i = 0; i < tmp.length; i++) tmp[i].remove();
-
-  let el = document.createElement('div');
-  el.setAttribute('id', 'antyslajd');
-  el.style.background = window.getComputedStyle(document.querySelector('body')).backgroundColor;
-
-  divs.forEach(div => {
-    div.style.display = 'inline-block';
-    div.style.clear = 'both';
-    div.style.margin = '20px auto';
-
-    const tmp = div.querySelectorAll(rule.removeFromContainer);
-    for (let i = 0; i < tmp.length; i++) tmp[i].remove();
-
-    const wrapper = document.createElement('div');
-    wrapper.setAttribute('class', 'antyslajd-wrapper');
-    wrapper.appendChild(div);
-
-    el.appendChild(wrapper);
-  });
-
-  document.querySelector('body').insertAdjacentElement('afterbegin', el);
-} 
-
-const rule = matchRule();
-if (rule) {
-  console.log('MATCHED', rule);
-  const next = document.querySelector(rule.nextSelector);
-  const nextUrl = next.getAttribute('href');
-  let htmls = [];
-  let seenUrls = [ nextUrl ];
-  if (next) {
-    load(seenUrls, htmls, rule, nextUrl);
-  }
-} else {
-  console.log('Antyslajd: nothing matched1');
-}
-*/
